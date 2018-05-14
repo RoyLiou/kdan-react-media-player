2a5cd79eb7cf779a86198a849262cef49c4bdf4b
RoyLiu <royliu@kdanmobile.com>
2018-05-14 15:18:07 +0800

    Remove package

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import sass from 'rollup-plugin-sass'
-import css from 'rollup-plugin-css-only'
 
 import pkg from './package.json'
 
@@ -26,18 +25,12 @@ export default {
     'prop-types',
     'redux'
   ],
-  globals: {
-    'video.js': 'videojs'
-  },
   plugins: [
     sass({
       output: 'dist/video-react.css',
       outputStyle: 'compressed',
       sourceMap: true,
       sourceMapContents: true,
-    }),
-    css({
-      output: 'dist/videojs.css',
     }),
     babel({
       exclude: 'node_modules/**',
